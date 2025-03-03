3f0bae1335fc28a4be677f3942fcda06bbf4ec7c
Mithun Boraiah <52660932+mithunboraiah007@users.noreply.github.com>
2025-03-03 12:13:05 +0530

    App.jsx

--- ReactApp/App.jsx
+++ ReactApp/App.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+import Card from "./components/Card"; // Adjust path based on your folder structure
 
+function App() {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
